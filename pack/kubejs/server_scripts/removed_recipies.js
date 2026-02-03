78c9efc5df1c3ada56d461f97336b429b322b4fc
acekssler16-glitch <acekssler16@gmail.com>
2026-02-03 16:52:07 -0600

    Delete pack directory

--- pack/kubejs/server_scripts/removed_recipies.js
+++
@@ -1,6 +0,0 @@
-// Removed Recipies
-ServerEvents.recipes(event => {
-// Replacements
-    // Compass (Made Cheaper)
-event.remove({ output: 'minecraft:compass' })
-})
--- pack/kubejs/startup_scripts/registry.js
+++
@@ -1,5 +0,0 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Items Registered! (Loaded startup registry script)')
-StartupEvents.registry('item', event => {
-  event.create('shimmer')
-})
